9ed43495ec242793d56e9e8e53c7f1d42ad54602
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-07-06 23:54:53 +0300

    Display and style categories

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //fetches categories from category API
+    //Fetches categories from category API
     fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
     .then(res => res.json())
     .then(data => displayCategories(data.categories))
     .catch(error => console.log(error));
 
+//Function to display categories
+    function displayCategories(categories) {
+        let categoriesElement = document.getElementById('categories');
+
+        categories.forEach(category => {
+        let categoryElement = document.createElement('div');
+        categoryElement.innerHTML = `
+            <img class="pic" src="${category.strCategoryThumb}" alt="${category.strCategory}">
+            <p class="cat">${category.strCategory}</p>`;
+        categoriesElement.appendChild(categoryElement);
+
+        categoryElement.addEventListener('click', () => categoryData(category));
+        });
+    }
 })
