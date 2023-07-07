dd95994fb4e64955e7dd6122b363218ec99ae1b4
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-07-07 14:20:21 +0300

    Add back button on the meals page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
 //Function to display categories
     function displayCategories(categories) {
         let categoriesElement = document.getElementById('categories');
+        categoriesElement.innerHTML = `
+        <h2>Categories</h2>`
 
         categories.forEach(category => {
         let categoryElement = document.createElement('div')
@@ -17,31 +19,56 @@ document.addEventListener('DOMContentLoaded', () => {
             <p><strong>Description:</strong>${category.strCategoryDescription}</p><br>`
         categoriesElement.appendChild(categoryElement)
 
-        categoryElement.addEventListener('click', () => categoryData(category));
+        categoryElement.addEventListener('click', () => categoryData(category))
         })
     }
 
-//Fetch meals under each category
-function categoryData(category) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`)
-    .then(res => res.json())
-    .then(data => renderMeals(data))}
 
-function renderMeals(data){
-    let mealElement = document.getElementById('categories')
+// Add event listener to the back button
+document.getElementById('backButton').addEventListener('click', () => {
+    // Clear the category meals element
+    document.getElementById('category-meals').innerHTML = '';
+
+    // Show the categories view
+    document.getElementById('categories').style.display = 'block';
+
+    // Hide the back button
+    document.getElementById('backButton').style.display = 'none';
+  });
+
+  // Fetch meals under each category
+  function categoryData(category) {
+    // Hide the categories view
+    document.getElementById('categories').style.display = 'none';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`)
+      .then(res => res.json())
+      .then(data => renderMeals(data));
+
+    // Show the back button
+    document.getElementById('backButton').style.display = 'block';
+  }
+
+  // Function to render meals
+  function renderMeals(data) {
+    let mealElement = document.getElementById('category-meals');
 
     mealElement.innerHTML = `
-    <h2 class="category-meals">Meals</h2><br>`
+      <h2 class="category-meals">Meals</h2><br>`;
 
     data.meals.forEach(meal => {
-    let mealItem = document.createElement('div');
-    mealItem.innerHTML = `
+      let mealItem = document.createElement('div');
+      mealItem.innerHTML = `
         <img class="meal-img" src="${meal.strMealThumb}" alt="${meal.strMeal}">
         <p id="meal-name">${meal.strMeal}</p>
-        `
-    mealElement.appendChild(mealItem)
-    })
-}
+      `;
+      mealElement.appendChild(mealItem);
+    });
+  }
+
+  // Hide the back button initially (in case it's not on the meals page)
+  document.getElementById('backButton').style.display = 'none';
+
 
 //Function to search for a meal using the search bar
     function searchMeal(){
